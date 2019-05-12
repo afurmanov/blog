48a30c609345dca1c1fe9744fce6c32e06a2c34d
Aleksandr Furmanov <aleksandr.furmanov@gmail.com>
2019-05-12 11:56:18 -0600

    Dependencies updated

--- app.js
+++ app.js
@@ -4,10 +4,15 @@
 
 var browserify = require('browserify-middleware');
 var express = require('express');
+var morgan = require('morgan')
+var favicon = require('serve-favicon')
+var bodyParser = require('body-parser')
+var errorHandler = require('errorhandler')
 var routes = require('./routes');
 var http = require('http');
+// var methodOverride = require('method-override');
 var path = require('path');
-var sass = require('node-sass');
+var sass = require('node-sass-middleware');
 
 var app = express();
 
@@ -15,29 +20,31 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use( sass.middleware({ src: __dirname + '/client',
+
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// app.use(express.methodOverride());
+//app.use(app.router);
+
+app.get('/', routes.index);
+app.get('/blog/articles', routes.articles(path.join(__dirname, 'articles')));
+app.get('/blog/articles/:article', routes.article(path.join(__dirname, 'articles')));
+app.get('/blog/*', routes.index);
+app.use( sass({ src: __dirname + '/client',
     dest: __dirname + '/public',
     debug: true
     }));
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+
 app.use('/javascripts', browserify('./client/javascripts'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorHandler);
 }
 
-app.get('/', routes.index);
-app.get('/blog/articles', routes.articles(path.join(__dirname, 'articles')));
-app.get('/blog/articles/:article', routes.article(path.join(__dirname, 'articles')));
-
-app.get('/blog/*', routes.index);
 
 require('./routes/redirects').init(app);
 
